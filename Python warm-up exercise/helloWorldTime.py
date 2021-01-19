60e3178c46f7479b903605e835543eb6f0e73e43
zwenact <45642009+zwenact@users.noreply.github.com>
2021-01-19 14:10:45 -0500

    seperated time.time()

--- Python warm-up exercise/helloWorldTime.py
+++ Python warm-up exercise/helloWorldTime.py
@@ -13,12 +13,12 @@ print("hello, world!")
 #Import time library to experiment time related functions.
 import time
 
+#Return the current time as seconds in GMT time zone. (time since 1970, January 01)
+print(time.time())
 
-#Concatenating three parameters, including the current time as seconds
+#Concatenating three parameters
 print("Since 1970, Jan 01 until now:", time.time(), "seconds have passed.")
 
-
 #A short program that converts the time stamp into the current local time.
-
 currentTime = time.time()
 print("Local time is", time.ctime(currentTime))
